00571a67e17083c74ff810516abeceee090fc9b3
Isac Lee <isl017@ucsd.edu>
2020-12-14 21:43:34 -0800

    guiding questions

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,6 @@ function init() {
     plotPie();
     plotHeat();
     plotBar();
-    linePlot();
 }
 
 // Stacked Bar Chart of Top 5 Call Emergencies 
@@ -811,6 +810,7 @@ function plotBar(){
                 align: 'middle'
             },
             labels: {
+                // enabled: false,
                 overflow: 'justify'
             }
         },
@@ -1168,3 +1168,31 @@ function linePlot(){
 //         }],
 //     });
 // }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
